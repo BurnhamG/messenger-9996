884f7db0c0768ba5d53ddce1d857bc2134ebec6f
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2021-08-24 15:27:20 -0500

    Fix inefficient message sorting

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,7 +8,7 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages.sort((first, second) => first.id - second.id).map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
