980155af0b16760cbfd79ab1396f202f0de36ad2
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2021-08-30 13:03:41 -0500

    Move login styling to a separate file

--- client/src/Login.js
+++ client/src/Login.js
@@ -12,49 +12,12 @@ import {
   IconButton,
   Link
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import { login } from "./store/utils/thunkCreators";
 import { WelcomeSideBanner } from "./WelcomeSideBanner.js";
 import "./Login.css";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "row",
-    alignContent: "center",
-    height: "100%"
-  },
-  textField: {
-    width: "30ch",
-  },
-  accountCreation: {
-    marginRight: "20px",
-    marginTop: "-30px",
-    color: "#777777"
-  },
-  button: {
-    background: "#FFFFFF",
-    color: "#3A8DFF",
-    padding: "10px 20px",
-    fontFamily: "Montserrat, sans-serif"
-  },
-  link: {
-    margin: "0px 10px 10px",
-    fontSize: ".75em"
-  },
-  welcome: {
-    marginLeft: "-110px",
-    fontSize: "1.25em",
-    fontWeight: "bold"
-  },
-  loginButton: {
-    fontFamily: "Montserrat, sans-serif",
-    marginTop: "10px"
-  }
-}));
+import { useStyles } from "./welcomeStyles.js";
 
 const Login = (props) => {
   const history = useHistory();
---
+++ client/src/welcomeStyles.js
@@ -0,0 +1,40 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    alignContent: "center",
+    height: "100%"
+  },
+  textField: {
+    width: "30ch",
+  },
+  accountCreation: {
+    marginRight: "20px",
+    marginTop: "-30px",
+    color: "#777777"
+  },
+  button: {
+    background: "#FFFFFF",
+    color: "#3A8DFF",
+    padding: "10px 20px",
+    fontFamily: "Montserrat, sans-serif"
+  },
+  link: {
+    margin: "0px 10px 10px",
+    fontSize: ".75em"
+  },
+  welcome: {
+    marginLeft: "-110px",
+    fontSize: "1.25em",
+    fontWeight: "bold"
+  },
+  loginButton: {
+    fontFamily: "Montserrat, sans-serif",
+    marginTop: "10px"
+  }
+}));
+
+export { useStyles };
