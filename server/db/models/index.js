169156144291ab88748e0be023a4ef353975e53c
BurnhamG <18239224+BurnhamG@users.noreply.github.com>
2021-09-04 19:36:47 -0500

    Make database model changes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,14 +4,13 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, { through: "ConversationUsers" });
+Conversation.belongsToMany(User, { through: "ConversationUsers" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
